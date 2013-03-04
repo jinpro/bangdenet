533cf27e9c1e6332059f2fbec15f1acce61bdbcf
jin <apple@appletekiMacBook-Pro.local>
2013-03-04 08:42:35 +0800

    Initial Commit

---
+++ bangdenet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bangdenet
+//
+//  Created by jin on 13-3-4.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bangdenet/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  bangdenet
+//
+//  Created by jin on 13-3-4.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
